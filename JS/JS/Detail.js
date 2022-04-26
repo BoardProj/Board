906307a62abf67f0f98535446ca1f3181548052d
heeheesun <heeheesun00@gmail.com>
2022-04-26 17:25:26 +0900

    add main update

---
+++ JS/JS/Detail.js
@@ -0,0 +1,155 @@
+const login = document.createElement('button');
+login.style.display="inline-block";
+login.innerHTML="로그인";
+
+const register = document.createElement('button');
+register.style.display="inline-block";
+register.innerHTML="회원가입";
+
+
+const detail = document.createElement('p');
+detail.innerHTML="회원정보";
+
+
+const hr = document.createElement('hr');
+
+
+const br = document.createElement('br');
+
+
+const name_1 = document.createElement('p');
+name_1.style.display="inline-block";
+name_1.innerHTML="이름";
+
+const name_2 = document.createElement('p');
+name_2.style.display="inline-block";
+name_2.innerHTML="****";
+
+const name_button = document.createElement('button');
+name_button.style.display="inline-block";
+name_button.innerHTML="변경";
+
+
+const br_2 = document.createElement('br');
+
+
+const id_1 = document.createElement('p');
+id_1.style.display="inline-block";
+id_1.innerHTML="아이디";
+
+const id_2 = document.createElement('p');
+id_2.style.display="inline-block";
+id_2.innerHTML="****";
+
+const id_button = document.createElement('button');
+id_button.style.display="inline-block";
+id_button.innerHTML="변경";
+
+
+const br_3 = document.createElement('br');
+
+
+const password_1 = document.createElement('p');
+password_1.style.display="inline-block";
+password_1.innerHTML="비밀번호";
+
+const password_2 = document.createElement('p');
+password_2.style.display="inline-block";
+password_2.innerHTML="****";
+
+const password_button = document.createElement('button');
+password_button.style.display="inline-block";
+password_button.innerHTML="변경";
+
+
+const br_4 = document.createElement('br');
+
+
+const email_1 = document.createElement('p');
+email_1.style.display="inline-block";
+email_1.innerHTML="이메일";
+
+const email_2 = document.createElement('p');
+email_2.style.display="inline-block";
+email_2.innerHTML="****";
+
+const email_button = document.createElement('button');
+email_button.style.display="inline-block";
+email_button.innerHTML="변경";
+
+
+const br_5 = document.createElement('br');
+
+
+const grade_1 = document.createElement('p');
+grade_1.style.display="inline-block";
+grade_1.innerHTML="회원등급";
+
+const grade_2 = document.createElement('p');
+grade_2.style.display="inline-block";
+grade_2.innerHTML="****";
+
+
+const hr_2 = document.createElement('hr');
+
+
+const main_button = document.createElement('button');
+main_button.innerHTML="메인으로";
+
+
+document.querySelector('body').appendChild(login);
+document.querySelector('body').appendChild(register);
+
+
+document.querySelector('body').appendChild(detail);
+
+
+document.querySelector('body').appendChild(br);
+
+
+document.querySelector('body').appendChild(hr);
+
+
+document.querySelector('body').appendChild(name_1);
+document.querySelector('body').appendChild(name_2);
+document.querySelector('body').appendChild(name_button);
+
+
+document.querySelector('body').appendChild(br_2);
+
+
+document.querySelector('body').appendChild(id_1);
+document.querySelector('body').appendChild(id_2);
+document.querySelector('body').appendChild(id_button);
+
+
+document.querySelector('body').appendChild(br_3);
+
+
+document.querySelector('body').appendChild(password_1);
+document.querySelector('body').appendChild(password_2);
+document.querySelector('body').appendChild(password_button);
+
+
+document.querySelector('body').appendChild(br_4);
+
+
+document.querySelector('body').appendChild(email_1);
+document.querySelector('body').appendChild(email_2);
+document.querySelector('body').appendChild(email_button);
+
+
+document.querySelector('body').appendChild(br_5);
+
+
+document.querySelector('body').appendChild(grade_1);
+document.querySelector('body').appendChild(grade_2);
+
+
+document.querySelector('body').appendChild(hr_2);
+
+
+document.querySelector('body').appendChild(main_button);
+
+
+
