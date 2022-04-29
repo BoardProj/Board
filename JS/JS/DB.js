ffb68a20124b83860a112a800cab00f16a6d4ba9
ParkSungCheol <skfo8gmlakd@naver.com>
2022-04-29 22:45:18 +0900

    Ver#2.4

--- JS/JS/DB.js
+++ JS/JS/DB.js
@@ -92,6 +92,34 @@ app.put('/test', (req, res) => {
     
 });
 
+app.post('/test', (req, res) => {
+    const conn = {
+        host : '101.101.211.124',
+        port : '3306',
+        user: 'root',
+        password : 'zoavjtm7#',
+        database : 'Board'
+    }
+    var connection = mysql.createConnection(conn); // DB 커넥션 생성
+    connection.connect();   // DB 접속
+    
+    let data = JSON.parse(req.headers.data);
+    let statement = "insert into user_info(user_id, user_password, user_name, user_email, user_status) values(";
+    statement += data.id + ", " + data.pw + ", " + data.name + ", '" + data.email + "', 0);";
+    
+    console.log(statement);
+    connection.query(statement, function (err, results, fields) { // testQuery 실행
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+        res.json(results);
+    });
+    
+    connection.end(); // DB 접속 종료
+    
+});
+
 app.listen(port, () => {
     console.log(`server is listening at localhost:${port}`);
 });
